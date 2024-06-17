d797cb1f98beae03aea2d96c9a5dd9584d5f7b71
uqonstudio <sdesain25@gmail.com>
2024-06-18 00:02:14 +0700

    add config connectD

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module submission-project-enigma-laundry
+module challenge-goapi
 
 go 1.19
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	_ "github.com/lib/pq"
+
+	"challenge-goapi/config"
+)
+
+var db = config.ConnectDB()
+
 func main() {
 	// Tulis kode kamu disini
-
+	// db := connectDB()
+	defer db.Close()
 }
